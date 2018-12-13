59470a10973fa2631dd402f4f81134cd0f139f8f
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-12-13 15:43:44 +0100

    passed second test, added if statements

--- geneseq.py
+++ geneseq.py
@@ -1,8 +1,21 @@
 
 
 def reverse_complement(seq):
-    return 'AGC'
+    rseq = []
+    for nt in seq:
+        if nt == 'G':
+            rseq.append('C')
+        elif nt == 'C':
+            rseq.append('G')
+        elif nt == 'T':
+            rseq.append('A')
+        elif nt == 'A':
+            rseq.append('T')
+    return ''.join(rseq)[::-1]
 
 
 def test_complment():
     assert reverse_complement('GCT') == 'AGC'
+
+def test_complement2():
+    assert reverse_complement('GGG') != 'AGC'
