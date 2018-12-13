d7f9cbdfe9052318bd61b5552a569ea107adef8e
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-12-13 16:08:19 +0100

    commit

--- geneseq.py
+++ geneseq.py
@@ -3,7 +3,17 @@ complements = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'T'}
 
 
 def reverse_complement(seq):
+    """
+    Returns the reversed complement of a sequence
+
+    Args:
+        seq (str): the sequence
+
+    Returns: the reversed complement
+
+    """
     seq = seq.upper()
+
     rseq = []
     for nt in seq:
         rseq.append(complements[nt])
